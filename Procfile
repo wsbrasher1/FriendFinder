aa7744150410fa6f206670b0c66c7357dac3ddbf
Bill Brasher <wsbrasher1@msn.com>
2019-06-09 19:19:37 -0500

    changed port and added Procfile

---
+++ Procfile
@@ -0,0 +1,7 @@
+web: node server.js
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+ port = 8080;
+}
+app.listen(port);
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require("path");
 
 //Express configuration//
 var app = express();
-var PORT = process.env.PORT || 3306;
+var PORT = process.env.PORT || 8080;
 
 //Sets up the Express app to handle data parsing//
 app.use(express.urlencoded({extended: true}));
