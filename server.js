46d059e826dd8a7ec0ac664621bb2169e8a88e7b
Bill Brasher <wsbrasher1@msn.com>
2019-06-11 21:41:07 -0500

    corrected path to css

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var PORT = process.env.PORT || 8080;
 //Sets up the Express app to handle data parsing//
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static('app/public'))
 
 //Routes - points the server to several route files//
 require("./app/routes/apiRoutes.js")(app);
